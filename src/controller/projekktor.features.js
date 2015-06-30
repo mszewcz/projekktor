b2b566bcf58df2c55e39c3ec55983383f7e6fc7f
Radosław Włodkowski <radoslaw@wlodkowski.net>
2015-06-30 15:59:08 +0200

    Add initial version of feature detection based on Modernizr 2.8.3

---
+++ src/controller/projekktor.features.js
@@ -0,0 +1,302 @@
+/*
+ * this file is part of: 
+ * projekktor zwei
+ * http://www.projekktor.com 
+ *
+ * Copyright 2015 Radosław Włodkowski, radoslaw@wlodkowski.net
+ * under GNU General Public License
+ * http://www.filenew.org/projekktor/license/
+ * 
+ * Code borrowed from:
+ * Modernizr 2.8.3 (Custom Build) | MIT & BSD
+ *
+ */
+jQuery(function ($) {
+    
+$p.features = (function (window, document, undefined) {
+
+    var Modernizr = {},
+        docElement = document.documentElement,
+        mod = 'modernizr',
+        modElem = document.createElement(mod),
+        mStyle = modElem.style,
+        inputElem,
+        toString = {}.toString,
+        prefixes = ' -webkit- -moz- -o- -ms- '.split(' '),
+        omPrefixes = 'Webkit Moz O ms',
+        cssomPrefixes = omPrefixes.split(' '),
+        domPrefixes = omPrefixes.toLowerCase().split(' '),
+        ns = {
+            'svg': 'http://www.w3.org/2000/svg'
+        },
+        tests = {},
+        inputs = {},
+        attrs = {},
+        classes = [],
+        slice = classes.slice,
+        featureName,
+        injectElementWithStyles = function (rule, callback, nodes, testnames) {
+
+            var style, ret, node, docOverflow,
+                div = document.createElement('div'),
+                body = document.body,
+                fakeBody = body || document.createElement('body');
+
+            if (parseInt(nodes, 10)) {
+                while (nodes--) {
+                    node = document.createElement('div');
+                    node.id = testnames ? testnames[nodes] : mod + (nodes + 1);
+                    div.appendChild(node);
+                }
+            }
+
+            style = ['&#173;', '<style id="s', mod, '">', rule, '</style>'].join('');
+            div.id = mod;
+            (body ? div : fakeBody).innerHTML += style;
+            fakeBody.appendChild(div);
+            if (!body) {
+                fakeBody.style.background = '';
+                fakeBody.style.overflow = 'hidden';
+                docOverflow = docElement.style.overflow;
+                docElement.style.overflow = 'hidden';
+                docElement.appendChild(fakeBody);
+            }
+
+            ret = callback(div, rule);
+            if (!body) {
+                fakeBody.parentNode.removeChild(fakeBody);
+                docElement.style.overflow = docOverflow;
+            } else {
+                div.parentNode.removeChild(div);
+            }
+
+            return !!ret;
+
+        },
+        isEventSupported = (function () {
+
+            var TAGNAMES = {
+                'select': 'input',
+                'change': 'input',
+                'submit': 'form',
+                'reset': 'form',
+                'error': 'img',
+                'load': 'img',
+                'abort': 'img'
+            };
+
+            function isEventSupported (eventName, element) {
+
+                element = element || document.createElement(TAGNAMES[eventName] || 'div');
+                eventName = 'on' + eventName;
+
+                var isSupported = eventName in element;
+
+                if (!isSupported) {
+                    if (!element.setAttribute) {
+                        element = document.createElement('div');
+                    }
+                    if (element.setAttribute && element.removeAttribute) {
+                        element.setAttribute(eventName, '');
+                        isSupported = is(element[eventName], 'function');
+
+                        if (!is(element[eventName], 'undefined')) {
+                            element[eventName] = undefined;
+                        }
+                        element.removeAttribute(eventName);
+                    }
+                }
+
+                element = null;
+                return isSupported;
+            }
+            return isEventSupported;
+        })(),
+        _hasOwnProperty = ({}).hasOwnProperty, hasOwnProp;
+        
+    Modernizr._prefixes = prefixes;
+    Modernizr._domPrefixes = domPrefixes;
+    Modernizr._cssomPrefixes = cssomPrefixes;
+    Modernizr.hasEvent = isEventSupported;
+    Modernizr.testProp = function (prop) {
+        return testProps([prop]);
+    };
+    Modernizr.testAllProps = testPropsAll;
+    Modernizr.testStyles = injectElementWithStyles;
+    Modernizr.prefixed = function (prop, obj, elem) {
+        if (!obj) {
+            return testPropsAll(prop, 'pfx');
+        } else {
+            return testPropsAll(prop, obj, elem);
+        }
+    };
+
+    if (!is(_hasOwnProperty, 'undefined') && !is(_hasOwnProperty.call, 'undefined')) {
+        hasOwnProp = function (object, property) {
+            return _hasOwnProperty.call(object, property);
+        };
+    }
+    else {
+        hasOwnProp = function (object, property) {
+            return ((property in object) && is(object.constructor.prototype[property], 'undefined'));
+        };
+    }
+
+    if (!Function.prototype.bind) {
+        Function.prototype.bind = function bind (that) {
+
+            var target = this;
+
+            if (typeof target != "function") {
+                throw new TypeError();
+            }
+
+            var args = slice.call(arguments, 1),
+                bound = function () {
+
+                    if (this instanceof bound) {
+
+                        var F = function () {
+                        };
+                        F.prototype = target.prototype;
+                        var self = new F();
+
+                        var result = target.apply(
+                            self,
+                            args.concat(slice.call(arguments))
+                            );
+                        if (Object(result) === result) {
+                            return result;
+                        }
+                        return self;
+
+                    } else {
+
+                        return target.apply(
+                            that,
+                            args.concat(slice.call(arguments))
+                            );
+
+                    }
+
+                };
+
+            return bound;
+        };
+    }
+
+    function setCss (str) {
+        mStyle.cssText = str;
+    }
+
+    function setCssAll (str1, str2) {
+        return setCss(prefixes.join(str1 + ';') + (str2 || ''));
+    }
+
+    function is (obj, type) {
+        return typeof obj === type;
+    }
+
+    function contains (str, substr) {
+        return !!~('' + str).indexOf(substr);
+    }
+
+    function testProps (props, prefixed) {
+        for (var i in props) {
+            var prop = props[i];
+            if (!contains(prop, "-") && mStyle[prop] !== undefined) {
+                return prefixed == 'pfx' ? prop : true;
+            }
+        }
+        return false;
+    }
+
+    function testDOMProps (props, obj, elem) {
+        for (var i in props) {
+            var item = obj[props[i]];
+            if (item !== undefined) {
+
+                if (elem === false)
+                    return props[i];
+
+                if (is(item, 'function')) {
+                    return item.bind(elem || obj);
+                }
+
+                return item;
+            }
+        }
+        return false;
+    }
+
+    function testPropsAll (prop, prefixed, elem) {
+
+        var ucProp = prop.charAt(0).toUpperCase() + prop.slice(1),
+            props = (prop + ' ' + cssomPrefixes.join(ucProp + ' ') + ucProp).split(' ');
+
+        if (is(prefixed, "string") || is(prefixed, "undefined")) {
+            return testProps(props, prefixed);
+
+        } else {
+            props = (prop + ' ' + (domPrefixes).join(ucProp + ' ') + ucProp).split(' ');
+            return testDOMProps(props, prefixed, elem);
+        }
+    }
+
+    tests['canvas'] = function () {
+        var elem = document.createElement('canvas');
+        return !!(elem.getContext && elem.getContext('2d'));
+    };
+
+    tests['canvastext'] = function () {
+        return !!(Modernizr['canvas'] && is(document.createElement('canvas')
+            .getContext('2d').fillText, 'function'));
+    };
+    tests['touch'] = function () {
+        var bool;
+
+        if (('ontouchstart' in window) || window.DocumentTouch && document instanceof DocumentTouch) {
+            bool = true;
+        } else {
+            injectElementWithStyles(['@media (', prefixes.join('touch-enabled),('), mod, ')',
+                '{#modernizr{top:9px;position:absolute}}'].join(''), function (node) {
+                bool = node.offsetTop === 9;
+            });
+        }
+
+        return bool;
+    };
+    tests['svg'] = function () {
+        return !!document.createElementNS && !!document.createElementNS(ns.svg, 'svg').createSVGRect;
+    };
+
+    tests['inlinesvg'] = function () {
+        var div = document.createElement('div');
+        div.innerHTML = '<svg/>';
+        return (div.firstChild && div.firstChild.namespaceURI) == ns.svg;
+    };
+    
+    tests['fullscreen'] = function () {
+        for (var i = 0; i < Modernizr._domPrefixes.length; i++) {
+            if (document[Modernizr._domPrefixes[i].toLowerCase() + 'CancelFullScreen'])
+                return true;
+        }
+        return !!document['cancelFullScreen'] || false;
+    }
+    
+    for (var feature in tests) {
+        if (hasOwnProp(tests, feature)) {
+            featureName = feature.toLowerCase();
+            Modernizr[featureName] = tests[feature]();
+
+            classes.push((Modernizr[featureName] ? '' : 'no-') + featureName);
+        }
+    }
+
+    setCss('');
+    modElem = inputElem = null;
+
+    return Modernizr;
+
+})(window, window.document);
+});
