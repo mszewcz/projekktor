e55343a4f036930e840387db32ed954c0c70ad30
Radosław Włodkowski <radoslaw@wlodkowski.net>
2017-07-27 00:46:36 +0200

    Add ES6 collections polyfill

--- Gruntfile.js
+++ Gruntfile.js
@@ -57,7 +57,7 @@ module.exports = function (grunt) {
     polyfiller: {
       build: {
         options: {
-          features: ['Promise', 'PointerEvents']
+          features: ['Promise', 'PointerEvents', 'Collections']
         },
         dest: 'dist/polyfills.js'
       }
