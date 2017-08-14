acf0b9da3ab33098b95bc0aaf441987223e140f4
Radosław Włodkowski <radoslaw@wlodkowski.net>
2017-08-14 06:41:08 +0200

    Cache detected plugin versions

--- src/controller/projekktor.platforms.js
+++ src/controller/projekktor.platforms.js
@@ -12,7 +12,9 @@
 
 (function(window, document, $, $p){
 
-    var testVideoEl = document.createElement('video');
+    var testVideoEl = document.createElement('video'),
+    silverlightVer,
+    flashVer;
 
     $p.platforms = {
         videojs: function() {
@@ -28,45 +30,49 @@
 
         /* returns the version of the flash player */
         flash: function () {
-            var result = $p.utils.detectPlugin('Shockwave Flash', 'application/x-shockwave-flash', 'ShockwaveFlash.ShockwaveFlash', function (ax) {
-                // adapted from SWFObject
-                var version = [],
-                    d = ax.GetVariable("$version");
-                if (d) {
-                    d = d.split(" ")[1].split(",");
-                    version = [parseInt(d[0], 10), parseInt(d[1], 10), parseInt(d[2], 10)];
-                }
-                return version;
-            });
+            if (!flashVer) {
+                flashVer = $p.utils.detectPlugin('Shockwave Flash', 'application/x-shockwave-flash', 'ShockwaveFlash.ShockwaveFlash', function (ax) {
+                    // adapted from SWFObject
+                    var version = [],
+                        d = ax.GetVariable("$version");
+                    if (d) {
+                        d = d.split(" ")[1].split(",");
+                        version = [parseInt(d[0], 10), parseInt(d[1], 10), parseInt(d[2], 10)];
+                    }
+                    return version;
+                }).join(".");
+            }
 
-            return result.join(".");
+            return flashVer;
         },
 
         silverlight: function () {
-            var result = $p.utils.detectPlugin('Silverlight Plug-In', 'application/x-silverlight-2', 'AgControl.AgControl', function (ax) {
-                // Silverlight cannot report its version number to IE
-                // but it does have a isVersionSupported function, so we have to loop through it to get a version number.
-                // adapted from http://www.silverlightversion.com/
-                var v = [0, 0, 0, 0],
-                    loopMatch = function (ax, v, i, n) {
-                        while (ax.isVersionSupported(v[0] + "." + v[1] + "." + v[2] + "." + v[3])) {
-                            v[i] += n;
-                        }
-                        v[i] -= n;
-                    };
-                loopMatch(ax, v, 0, 1);
-                loopMatch(ax, v, 1, 1);
-                loopMatch(ax, v, 2, 10000); // the third place in the version number is usually 5 digits (4.0.xxxxx)
-                loopMatch(ax, v, 2, 1000);
-                loopMatch(ax, v, 2, 100);
-                loopMatch(ax, v, 2, 10);
-                loopMatch(ax, v, 2, 1);
-                loopMatch(ax, v, 3, 1);
+            if (!silverlightVer) {
+                silverlightVer = $p.utils.detectPlugin('Silverlight Plug-In', 'application/x-silverlight-2', 'AgControl.AgControl', function (ax) {
+                    // Silverlight cannot report its version number to IE
+                    // but it does have a isVersionSupported function, so we have to loop through it to get a version number.
+                    // adapted from http://www.silverlightversion.com/
+                    var v = [0, 0, 0, 0],
+                        loopMatch = function (ax, v, i, n) {
+                            while (ax.isVersionSupported(v[0] + "." + v[1] + "." + v[2] + "." + v[3])) {
+                                v[i] += n;
+                            }
+                            v[i] -= n;
+                        };
+                    loopMatch(ax, v, 0, 1);
+                    loopMatch(ax, v, 1, 1);
+                    loopMatch(ax, v, 2, 10000); // the third place in the version number is usually 5 digits (4.0.xxxxx)
+                    loopMatch(ax, v, 2, 1000);
+                    loopMatch(ax, v, 2, 100);
+                    loopMatch(ax, v, 2, 10);
+                    loopMatch(ax, v, 2, 1);
+                    loopMatch(ax, v, 3, 1);
 
-                return v;
-            });
+                    return v;
+                }).join(".");
+            }
+            return silverlightVer;
 
-            return result.join(".");
         },
 
         android: function () {
@@ -95,7 +101,7 @@
                         return "1";
             }
         },
-
+        
         browser: function () {
             return "1";
         }
