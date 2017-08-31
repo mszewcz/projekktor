4664ceceb9a23e6d841819ae819cb70617f17ea7
Radosław Włodkowski <radoslaw@wlodkowski.net>
2017-08-31 17:38:48 +0200

    Bump version to 1.7.2

--- src/controller/projekktor.config.version.js
+++ src/controller/projekktor.config.version.js
@@ -3,7 +3,7 @@ var projekktorConfig = (function (window, document, $, $p){
     "use strict";
 
 function projekktorConfig() {
-    this._version = "1.7.1";
+    this._version = "1.7.2";
 }
 
 return projekktorConfig;
