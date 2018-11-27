cdc3fa4b8b360c11bc65e999678c1ad320353682
Radosław Włodkowski <radoslaw.wlodkowski@intive.com>
2018-11-28 00:46:02 +0100

    Bump version to 1.8.1

--- src/controller/projekktor.config.version.js
+++ src/controller/projekktor.config.version.js
@@ -3,7 +3,7 @@ var projekktorConfig = (function (window, document, $, $p){
     "use strict";
 
 function projekktorConfig() {
-    this._version = "1.8.0";
+    this._version = "1.8.1";
 }
 
 return projekktorConfig;
