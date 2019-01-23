b1d422ffc853bdd20516ff2ea6b42f86c928895b
Radosław Włodkowski <radoslaw@wlodkowski.net>
2019-01-23 02:16:03 +0100

    Bump version to 1.8.2

--- src/controller/projekktor.config.version.js
+++ src/controller/projekktor.config.version.js
@@ -3,7 +3,7 @@ var projekktorConfig = (function (window, document, $, $p){
     "use strict";
 
 function projekktorConfig() {
-    this._version = "1.8.1";
+    this._version = "1.8.2";
 }
 
 return projekktorConfig;
